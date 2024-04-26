74808d758c9de7b7de8b7018b8bf4b3546d3e908
Dharunprakash <dharunprakashm21cs@psnacet.edu.in>
2024-04-26 12:08:29 +0530

    login form created

--- app/(Auth)/login/page.jsx
+++ app/(Auth)/login/page.jsx
@@ -1,29 +1,16 @@
-'use client'
-import { UploadDropzone } from '@uploadthing/react'
-import Error from 'next/error';
-import React from 'react'
+"use client";
+import LoginForm from "@/components/signinform";
+
+import React from "react";
 
 function page() {
   return (
     <div>
-      <h1>Login</h1>
-      <form>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UploadDropzone
-        endpoint="VehicleImage"
-        onClientUploadComplete={(res) => {
-         
-          console.log("Files: ", res);
-          alert("Upload Completed");
-        }}
-        onUploadError={(error) => {
-          alert(`ERROR! ${error.message}`);
-        }}
-      />
-    </main>
-      </form>
+      <div class="flex justify-center items-center h-screen ">
+        <LoginForm />
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
